a2c21da011e8a7d7068ec9a0f17afa0508939741
marinajulia <marinajulia707@gmail.com>
2023-06-17 18:15:06 -0300

    receive queue from rabbit

--- WeatherReportNotification/WeatherReportNotification/Program.cs
+++ WeatherReportNotification/WeatherReportNotification/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using WeatherReportNotification.WeatherReport;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "WeatherReport",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var objeto = JsonConvert.DeserializeObject<WeatherReportEntity>(message);
+
+                    Console.WriteLine($" [x] Recebida: {message}");
+                };
+
+                channel.BasicConsume(queue: "WeatherReport",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
