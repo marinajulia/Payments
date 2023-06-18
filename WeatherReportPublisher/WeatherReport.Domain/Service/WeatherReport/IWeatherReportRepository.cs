0f56bba79b4faee3e9e149e7a64b3b3e34f5999a
marinajulia <marinajulia707@gmail.com>
2023-06-17 21:19:20 -0300

    Removing files tracked by .gitignore

--- WeatherReportPublisher/WeatherReport.Domain/Service/WeatherReport/IWeatherReportRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using WeatherReport.Domain.Service.WeatherReport.Entities;
-
-namespace WeatherReport.Domain.Service.WeatherReport
-{
-    public interface IWeatherReportRepository
-    {
-        void PostWeather(WeatherReportEntity weatherReport); //tem que mandar as credenciais do usuario tbm
-    }
-}
