7e311410ebcf3ee01f1fc1ce97e5bca835c82f28
marinajulia <marinajulia707@gmail.com>
2023-07-22 11:08:57 -0300

    .gitignore fix

--- PublisherJob/PublisherJob/Program.cs
+++ PublisherJob/PublisherJob/Program.cs
@@ -6,33 +6,27 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        // Configura o agendador do Quartz
         var schedulerFactory = new StdSchedulerFactory();
         var scheduler = await schedulerFactory.GetScheduler();
         await scheduler.Start();
 
-        // Cria o JobDetail que define o job a ser executado
         var jobDetail = JobBuilder.Create<Job>()
-            .WithIdentity("Job", "grupoJobs")
+            .WithIdentity("Job", "Jobs")
             .Build();
 
-        // Cria o gatilho (trigger) que define a frequência de execução do job
         var trigger = TriggerBuilder.Create()
-            .WithIdentity("meuTrigger", "grupoTriggers")
+            .WithIdentity("Trigger", "Triggers")
             .StartNow()
             .WithSimpleSchedule(x => x
-                .WithIntervalInSeconds(10) // Intervalo de 10 segundos (altere conforme necessário)
+                .WithIntervalInSeconds(10) 
                 .RepeatForever())
             .Build();
 
-        // Agenda o job com o agendador
         await scheduler.ScheduleJob(jobDetail, trigger);
 
-        // Deixa a aplicação rodando para que o job seja executado
         Console.WriteLine("Job agendado. Pressione qualquer tecla para sair.");
         Console.ReadKey();
 
-        // Desliga o agendador ao encerrar a aplicação
         await scheduler.Shutdown();
     }
 }
